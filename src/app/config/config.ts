9a5d66b966c2456f8de9325ed0c366df03e2a6ec
Rodrigo Varas <rvaras@ceapro.cl>
2018-08-04 13:23:17 -0400

    Fichas html casi terminado

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-//export const URL_SERVICIOS = 'http://localhost:3977/';
-export const URL_SERVICIOS = 'http://142.93.24.11:3977/';
+export const URL_SERVICIOS = 'http://localhost:3977/';
+//export const URL_SERVICIOS = 'http://142.93.24.11:3977/';
