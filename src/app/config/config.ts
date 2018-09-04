fce48a66951b6b2dc05d20f944e707ec60332fa4
Rodrigo Varas <rvaras@ceapro.cl>
2018-09-04 20:01:54 -0300

    fin rev6

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,3 @@
-export const URL_SERVICIOS = 'http://localhost:3977/';
-//export const URL_SERVICIOS = 'http://142.93.24.11:3977/';
+//export const URL_SERVICIOS = 'http://localhost:3789/';
+export const URL_SERVICIOS = 'http://142.93.24.11:3789/';
+//export const URL_SERVICIOS = 'http://centropiecmds.cl:3789/';
